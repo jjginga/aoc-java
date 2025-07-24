bbf9b73432637a4ad7a1e069ca2086c8bd2dc0f9
Joel Jose Ginga <jjginga@gmail.com>
2025-07-24 09:55:31 +0100

    2015 - Added Day 10

--- src/test/java/com/jjginga/aoc/year2015/Day10Test.java
+++ src/test/java/com/jjginga/aoc/year2015/Day10Test.java
@@ -8,41 +8,40 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.List;
 
-import static com.jjginga.aoc.utils.Constants.DAY_09;
-import static com.jjginga.aoc.utils.Constants.YEAR_2015;
+import static com.jjginga.aoc.utils.Constants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Day09Test {
+public class Day10Test {
 
     private static Solution solution;
 
     @BeforeAll
     static void beforeAll() {
-        solution = new Day09();
+        solution = new Day10();
     }
 
     @Test
     void testPart1() throws IOException {
-        List<String> input = InputReader.readLines(YEAR_2015, DAY_09);
+        List<String> input = InputReader.readLines(YEAR_2015, DAY_10);
 
         long start = System.nanoTime();
         String result = solution.part1(input);
         long duration = System.nanoTime() - start;
-        System.out.printf("%s %s -> Part 1, result: %s | took %.2f ms%n", YEAR_2015, DAY_09, result, duration / 1_000_000.0);
+        System.out.printf("%s %s -> Part 1, result: %s | took %.2f ms%n", YEAR_2015, DAY_10, result, duration / 1_000_000.0);
 
-        assertEquals("207", result);
+        assertEquals("492982", result);
     }
 
     @Test
     void testPart2() throws IOException {
-        List<String> input = InputReader.readLines(YEAR_2015, DAY_09);
+        List<String> input = InputReader.readLines(YEAR_2015, DAY_10);
 
         long start = System.nanoTime();
         String result = solution.part2(input);
         long duration = System.nanoTime() - start;
-        System.out.printf("%s %s -> Part 2, result: %s | took %.2f ms%n", YEAR_2015, DAY_09, result, duration / 1_000_000.0);
+        System.out.printf("%s %s -> Part 2, result: %s | took %.2f ms%n", YEAR_2015, DAY_10, result, duration / 1_000_000.0);
 
-        assertEquals("804", result);
+        assertEquals("6989950", result);
     }
 
 
